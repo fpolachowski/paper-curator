2e23f5a18361732beae83bab41b266dfed6a8d55
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2025-02-13 22:39:21 +0100

    cleaned code

--- src/main/kotlin/de/fpolachowski/papercurator/service/DocumentService.kt
+++ src/main/kotlin/de/fpolachowski/papercurator/service/DocumentService.kt
@@ -2,17 +2,14 @@ package de.fpolachowski.papercurator.service
 
 import de.fpolachowski.papercurator.model.Document
 import de.fpolachowski.papercurator.repository.DocumentRepository
-import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.context.event.EventListener
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
+
 
 @Service
 class DocumentService(
     private val documentRepository: DocumentRepository
 ) {
-
     fun findAll(): MutableIterable<Document> {
         return documentRepository.findAll()
     }
