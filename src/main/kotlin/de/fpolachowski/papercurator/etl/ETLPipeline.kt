62a6bae71dc909f837b4b6e9cf5a86af4bfd705c
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2025-02-18 10:04:09 +0100

    added logging

--- src/main/kotlin/de/fpolachowski/papercurator/etl/ETLPipeline.kt
+++ src/main/kotlin/de/fpolachowski/papercurator/etl/ETLPipeline.kt
@@ -3,6 +3,7 @@ package de.fpolachowski.papercurator.etl
 import de.fpolachowski.papercurator.model.ContentType
 import de.fpolachowski.papercurator.model.Document
 import de.fpolachowski.papercurator.util.StringManipulator.Companion.cleanString
+import org.slf4j.LoggerFactory
 import org.springframework.ai.document.Document as AIDocument
 import org.springframework.ai.document.DocumentTransformer
 import org.springframework.ai.document.DocumentWriter
@@ -14,14 +15,18 @@ class ETLPipeline(
     private val documentTransformer: DocumentTransformer,
     private val documentWriter: DocumentWriter
 ) {
+    val logger = LoggerFactory.getLogger(javaClass)
 
     fun run(url : String) : List<Document> {
         val documents = documentRetriever.read(url)
         for (document in documents) {
             try {
+                logger.info("Processing document ${document.title.substring(0, 10)}")
                 val linkURL = document.urls.find { it.contentType == ContentType.PDF } ?: continue
                 val aiDocuments = documentReader.read(linkURL.url)
+                logger.info("Found document ${document.title.substring(0, 10)}")
                 val transformedDocuments = documentTransformer.apply(aiDocuments)
+                logger.info("Transformed document ${document.title.substring(0, 10)}")
 
                 // Clean the string and remove duplicate spaces
                 val finalDocuments = transformedDocuments.map { it.text?.let { text ->
@@ -32,6 +37,7 @@ class ETLPipeline(
                 } }
 
                 documentWriter.accept(finalDocuments)
+                logger.info("Saved documents ${document.title.substring(0, 10)}")
                 document.documents = finalDocuments.mapNotNull { it?.id }
                 Thread.sleep(500) //To not exceed the api threshold
             } catch (e: Exception) {
--- src/main/kotlin/de/fpolachowski/papercurator/etl/arxiv/ArxivDocumentRetrieval.kt
+++ src/main/kotlin/de/fpolachowski/papercurator/etl/arxiv/ArxivDocumentRetrieval.kt
@@ -5,8 +5,10 @@ import de.fpolachowski.papercurator.etl.ETLPipeline
 import de.fpolachowski.papercurator.etl.PdfDocumentReader
 import de.fpolachowski.papercurator.model.Document
 import de.fpolachowski.papercurator.repository.DocumentRepository
+import org.slf4j.LoggerFactory
 import org.springframework.ai.transformer.splitter.TokenTextSplitter
 import org.springframework.ai.vectorstore.VectorStore
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -16,8 +18,9 @@ import org.springframework.stereotype.Service
 @EnableScheduling
 class ArxivDocumentRetrieval(
     private val documentRepository: DocumentRepository,
-    private val vectorStore: VectorStore
+    private val vectorStore: PgVectorStore
 ) : DocumentRetrieval {
+    private val logger = LoggerFactory.getLogger(javaClass)
     private val defaultSort = "lastUpdatedDate"
     private val defaultOrder = "ascending"
     private val categories = listOf("cs")
@@ -35,22 +38,27 @@ class ArxivDocumentRetrieval(
         //    val documents = findAllByCategory(category)
         //    documentRepository.saveAll(documents)
         //}
+        logger.info("Retrieving daily documents")
 
-    val documents = findAllByTitle("electron")
-    documentRepository.saveAll(documents)
+        val documents = findAllByTitle("electron")
+        documentRepository.saveAll(documents)
+        logger.info("Retrieved ${documents.size} documents")
     }
 
     override fun findAllByCategory(category: String): List<Document> {
+        logger.info("Processing category: $category")
         val url = "https://rss.arxiv.org/atom/$category"
         return pipeline.run(url)
     }
 
     override fun findById(id: String): Document? {
+        logger.info("Processing document by id: $id")
         val url = "http://export.arxiv.org/api/query?search_query=id_list:${id}&sortBy=${this.defaultSort}&sortOrder=${this.defaultOrder}"
         return pipeline.run(url).firstOrNull()
     }
 
     override fun findAllByTitle(title: String): List<Document> {
+        logger.info("Processing document by title: $title")
         val url = "http://export.arxiv.org/api/query?search_query=ti:${title}&sortBy=${this.defaultSort}&sortOrder=${this.defaultOrder}"
         return pipeline.run(url)
     }
